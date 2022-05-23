b418ddefc0c328b1b838991c59e581699e71fd55
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-23 14:40:33 +0200

    aggiunta richiesta esercizio dadi

---
+++ dadi/js/app.js
@@ -0,0 +1,9 @@
+/*
+Richiesta dell'esercizio sui dadi:
+Gioco dei dadi
+Generare più  numeri random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+C’è forse un array da qualche parte?
+Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+*/
