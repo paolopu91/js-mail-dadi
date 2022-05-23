283f927d0e322e296caa4a91ccdfe9e2aa036a67
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-23 17:13:23 +0200

    aggiunte modifiche e finito esercizio dadi

--- dadi/js/app.js
+++ dadi/js/app.js
@@ -13,8 +13,7 @@ alert("Sei pronto a giocare un'emozionante partita a dadi?")
 alert("Prima di iniziare a giocare contro altri player, dovrai battere il computer")
 
 //Creiamo i due sfidanti
-const humanPlayer = document.getElementById("btn-human");
-const botPlayer = document.getElementById("btn-bot");
+const humanPlayer = document.getElementById("btn-play");
 
 // creiamo il primo array di numeri casuali per il nostro giocatore umano
 const numeriCasualiPlayer = [1,2,3,4,5,6];
@@ -22,6 +21,8 @@ const numeriCasualiPlayer = [1,2,3,4,5,6];
  // creiamo il secondo array per il nostro bot
 const numeriCasualiBot = [1,2,3,4,5,6];
 
+// creo la mia variabile di base per il colore del testo
+let colorTxt = ("color-black")
 
 //ora creiamo la condizine secondo cui l'utente cliccando genererà i numeri sulla pagina HTML
 humanPlayer.addEventListener("click", function() {
@@ -37,36 +38,51 @@ for(i = 1; i<numeriCasualiPlayer.length; i++){
 
     // creo la condizione per far si che l'evento si veda in html dopo aver premuto il button gioca
     console.log(numeriCasualiPlayer[i]);
-    let resultListHuman = document.querySelector(".result-list-human"),
+    const resultListHuman = document.querySelector(".result-list-human");
     resultListHuman.innerHTML ="";
-    resultListHuman.innerHTML = risultatoHuman;
-
-}
-
-
-})
-
-//ora creiamo la condizine secondo cui l'utente cliccando genererà i numeri sulla pagina HTML
-botPlayer.addEventListener("click", function(){
-    console.log("Pulsante Play cliccato");
-
-    console.log("BotNumber");
-for(i = 1 ; i<numeriCasualiBot.length; i++){
+    resultListHuman.innerHTML += risultatoHuman + " Numero Player";
+    
+    
+    for(i = 1 ; i<numeriCasualiBot.length; i++){
    
-    // creo le mie variabili per avere numeri casuali;
+    // creo le mie variabili per avere numeri casuali
     let randomBot = Math.floor(Math.random() * numeriCasualiBot.length);
     let risultatoBot = numeriCasualiBot[randomBot];
     console.log(risultatoBot);
 
     // creo la condizione per far si che l'evento si veda in html dopo aver premuto il button gioca
     console.log(numeriCasualiBot[i]);
-    let resultListBot = document.querySelector(".result-list-bot");
+    const resultListBot = document.querySelector(".result-list-bot");
     resultListBot.innerHTML="";
-    resultListBot.innerHTML = risultatoBot;
+    resultListBot.innerHTML += risultatoBot + " Numero Bot";
+
+    const finalResult = document.querySelector(".risultato-finale");
+    
+
+if(risultatoBot>risultatoHuman){
+    colorTxt = ("color-red")
+    finalResult.innerHTML = "Hai perso! riprova"
+    console.log("hai perso! Riprova")
+    
+}else if(risultatoBot<risultatoHuman){
+    colorTxt=("color-green")
+   finalResult.innerHTML = "Hai vinto!! Sei un campione"
+    console.log("Hai vinto!! Sei un campione")
+
+}else if(risultatoBot == risultatoHuman){
+    colorTxt=("color-purple")
+   finalResult.innerHTML = "Peccato eri così vicino!"
+    console.log("hai pareggiato")
+}
+
+}
 
 }
 
 })
+
+
+
 
 
 
